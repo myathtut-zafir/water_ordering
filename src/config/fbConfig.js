4a18a2560420c2f4a9ec1e57bdd2443c5d7c3d8b
myathtut-zafir <aa.htut@gmail.com>
2020-06-04 20:31:27 +0630

    [fix] - fix fb config

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -4,13 +4,13 @@ import "firebase/auth";
 
 // Replace this with your own config details
 let config = {
-    apiKey: "AIzaSyDG6SL5ChpAzKRxSLu_UYHy3-A2RA2efXM",
-    authDomain: "reacttest-ac34f.firebaseapp.com",
-    databaseURL: "https://reacttest-ac34f.firebaseio.com",
-    projectId: "reacttest-ac34f",
-    storageBucket: "reacttest-ac34f.appspot.com",
-    messagingSenderId: "466330380244",
-    appId: "1:466330380244:web:d757d16f2e057c47"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 let fbConfig = firebase.initializeApp(config);
